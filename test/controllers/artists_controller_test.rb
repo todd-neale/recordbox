e92d32ef616025f14743ae6ee2f5786e632cb060
Todd Neale <todd@todds-air.home>
2022-04-05 15:46:39 +0100

    heroku

---
+++ test/controllers/artists_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class ArtistsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @artist = artists(:one)
+  end
+
+  test "should get index" do
+    get artists_url
+    assert_response :success
+  end
+
+  test "should show artist" do
+    get artist_url(@artist)
+    assert_response :success
+  end
+
+end
